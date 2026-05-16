4bc0ecb13f6ae288c5d1c28136f28c71aa37cd80
lacc2201 <159913218+lacc2201@users.noreply.github.com>
2026-05-15 23:12:03 -0500

    postgres

--- papeleria_system/settings.py
+++ papeleria_system/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-80m%ai-p(d6#@a9e!k(kl!@uv&+%+nb*%q2v6c7)@-pkyrd301'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
